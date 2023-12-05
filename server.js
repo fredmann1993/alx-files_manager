46fa735632ea134655e0102e53848b42c6ce61b5
fredmann1993 <fasuyifisayo@gmail.com>
2023-12-05 09:20:14 +0100

    added files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import startServer from './libs/boot';
 import injectRoutes from './routes';
 import injectMiddlewares from './libs/middlewares';
 
+//create an express instance
 const server = express();
 
 injectMiddlewares(server);
